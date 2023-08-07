243ecdd219663079c0ec8c8701778223ae87ff26
zana-abdollahpour <zana.ap4@gmail.com>
2023-08-07 11:06:18 +0330

    add App Layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,30 @@
+import { Outlet } from "react-router-dom";
+import { styled } from "styled-components";
+
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+const StyledApp = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledApp>
+      <Sidebar />
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledApp>
+  );
+}
+export default AppLayout;
