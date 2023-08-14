cdfa5d6b913121a692fd5ca1150e5f36feb07298
zana-abdollahpour <zana.ap4@gmail.com>
2023-08-14 10:53:03 +0330

    implement checking in a booking functionality

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { format, isToday } from "date-fns";
 import { useNavigate } from "react-router-dom";
-import { HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
 
 import Tag from "../../ui/Tag";
 import Table from "../../ui/Table";
@@ -94,6 +94,15 @@ function BookingRow({
           >
             See Details
           </Menus.Button>
+
+          {status === "unconfirmed" && (
+            <Menus.Button
+              icon={<HiArrowDownOnSquare />}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Check in
+            </Menus.Button>
+          )}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
---
+++ src/features/check-in-out/useCheckin.js
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+
+import { updateBooking } from "../../services/apiBookings";
+
+export function useChecking() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
+    mutationFn: (bookingID) =>
+      updateBooking(bookingID, { status: "checked-in", isPaid: true }),
+
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} successfully checked in.`);
+      queryClient.invalidateQueries({ active: true });
+      navigate("/");
+    },
+
+    onError: () => toast.error("There was an error while checking in"),
+  });
+
+  return { checkin, isCheckingIn };
+}
