ee8223e5c60bf5d9901e869c99bf31660a167b78
zana-abdollahpour <zana.ap4@gmail.com>
2023-08-16 10:35:05 +0330

    implement updating user data and password

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -19,7 +19,7 @@ function UpdatePasswordForm() {
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow
-        label="Password (min 8 characters)"
+        label=" New password (min 8 chars)"
         error={errors?.password?.message}
       >
         <Input
@@ -38,7 +38,7 @@ function UpdatePasswordForm() {
       </FormRow>
 
       <FormRow
-        label="Confirm password"
+        label="Confirm new password"
         error={errors?.passwordConfirm?.message}
       >
         <Input
